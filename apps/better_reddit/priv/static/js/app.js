e5af611db286e7514be71c051ebe07217ad0e7b9
samdoiron <sam.doiron96@gmail.com>
2017-01-17 09:51:25 -0400

    Fix styling of post list on small browser screens

--- apps/better_reddit/priv/static/js/app.js
+++ apps/better_reddit/priv/static/js/app.js
@@ -7893,21 +7893,15 @@ require.register("web/static/elm/elm-stuff/packages/elm-lang/virtual-dom/2.0.3/t
 });
 
 ;require.register("web/static/js/app.js", function(exports, require, module) {
-'use strict';
+"use strict";
 
-require('phoenix_html');
+require("phoenix_html");
 
-var _elm = require('./elm.js');
-
-var Elm = _interopRequireWildcard(_elm);
-
-require('./socket.js');
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+require("./socket.js");
 
 });
 
-;require.register("web/static/js/elm.js", function(exports, require, module) {
+require.register("web/static/js/elm.js", function(exports, require, module) {
 'use strict';var _typeof=typeof Symbol==="function"&&typeof Symbol.iterator==="symbol"?function(obj){return typeof obj;}:function(obj){return obj&&typeof Symbol==="function"&&obj.constructor===Symbol?"symbol":typeof obj;};(function(){'use strict';function F2(fun){function wrapper(a){return function(b){return fun(a,b);};}wrapper.arity=2;wrapper.func=fun;return wrapper;}function F3(fun){function wrapper(a){return function(b){return function(c){return fun(a,b,c);};};}wrapper.arity=3;wrapper.func=fun;return wrapper;}function F4(fun){function wrapper(a){return function(b){return function(c){return function(d){return fun(a,b,c,d);};};};}wrapper.arity=4;wrapper.func=fun;return wrapper;}function F5(fun){function wrapper(a){return function(b){return function(c){return function(d){return function(e){return fun(a,b,c,d,e);};};};};}wrapper.arity=5;wrapper.func=fun;return wrapper;}function F6(fun){function wrapper(a){return function(b){return function(c){return function(d){return function(e){return function(f){return fun(a,b,c,d,e,f);};};};};};}wrapper.arity=6;wrapper.func=fun;return wrapper;}function F7(fun){function wrapper(a){return function(b){return function(c){return function(d){return function(e){return function(f){return function(g){return fun(a,b,c,d,e,f,g);};};};};};};}wrapper.arity=7;wrapper.func=fun;return wrapper;}function F8(fun){function wrapper(a){return function(b){return function(c){return function(d){return function(e){return function(f){return function(g){return function(h){return fun(a,b,c,d,e,f,g,h);};};};};};};};}wrapper.arity=8;wrapper.func=fun;return wrapper;}function F9(fun){function wrapper(a){return function(b){return function(c){return function(d){return function(e){return function(f){return function(g){return function(h){return function(i){return fun(a,b,c,d,e,f,g,h,i);};};};};};};};};}wrapper.arity=9;wrapper.func=fun;return wrapper;}function A2(fun,a,b){return fun.arity===2?fun.func(a,b):fun(a)(b);}function A3(fun,a,b,c){return fun.arity===3?fun.func(a,b,c):fun(a)(b)(c);}function A4(fun,a,b,c,d){return fun.arity===4?fun.func(a,b,c,d):fun(a)(b)(c)(d);}function A5(fun,a,b,c,d,e){return fun.arity===5?fun.func(a,b,c,d,e):fun(a)(b)(c)(d)(e);}function A6(fun,a,b,c,d,e,f){return fun.arity===6?fun.func(a,b,c,d,e,f):fun(a)(b)(c)(d)(e)(f);}function A7(fun,a,b,c,d,e,f,g){return fun.arity===7?fun.func(a,b,c,d,e,f,g):fun(a)(b)(c)(d)(e)(f)(g);}function A8(fun,a,b,c,d,e,f,g,h){return fun.arity===8?fun.func(a,b,c,d,e,f,g,h):fun(a)(b)(c)(d)(e)(f)(g)(h);}function A9(fun,a,b,c,d,e,f,g,h,i){return fun.arity===9?fun.func(a,b,c,d,e,f,g,h,i):fun(a)(b)(c)(d)(e)(f)(g)(h)(i);}//import Native.List //
 var _elm_lang$core$Native_Array=function(){// A RRB-Tree has two distinct data types.
 // Leaf -> "height"  is always 0
